18be492893f7e04c266b61a5d3de15398aebe441
Anton Ganhammar <ganhammar@gmail.com>
2017-04-01 20:59:17 +0200

    Failed login fix

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 
 import Loading from './../components/Loading';
+import Notification from './../components/Notification';
 import Me from './../api/Me';
 import TrackerStore from './../stores/TrackerStore';
 
@@ -11,6 +12,7 @@ class Login extends React.Component {
 
     this.state = {
       isLoading: false,
+      showHint: false,
       username: '',
       password: ''
     };
@@ -27,10 +29,12 @@ class Login extends React.Component {
   }
 
   handleUsernameChange(event) {
+    this.setState({showHint: false});
     this.setState({username: event.target.value});
   }
 
   handlePasswordChange(event) {
+    this.setState({showHint: false});
     this.setState({password: event.target.value});
   }
 
@@ -60,13 +64,20 @@ class Login extends React.Component {
         browserHistory.push('/project-list');
       })
       .catch(() => {
-
+        this.setState({ isLoading: false, showHint: true });
       });
   }
 
   render() {
+    let hint;
+
     if (this.state.isLoading) {
       return <Loading />;
+    }
+
+    if (this.state.showHint) {
+      hint = (<Notification level="error"
+        message="No user found with that username and password." />);
     }
 
     return (
@@ -75,6 +86,7 @@ class Login extends React.Component {
           onChange={this.handleUsernameChange.bind(this)} placeholder="Username" />
         <input type="password" value={this.state.password}
           onChange={this.handlePasswordChange.bind(this)} placeholder="Password" />
+        {hint}
         <input type="submit" value="Login" onClick={this.login.bind(this)} />
       </form>
     );
