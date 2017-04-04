12f7be63ca1e068178ab5aa846d9ee5637ed7fa4
Anton Ganhammar <anton.ganhammar@sca.com>
2017-04-04 07:20:01 +0200

    Story modal wip

--- src/components/SprintOverview/StoryModal.js
+++ src/components/SprintOverview/StoryModal.js
@@ -5,25 +5,46 @@ class StoryModal extends Component {
     super();
 
     this.state = {
-      name: ''
+      name: '',
+      description: '',
+      isEditingDescription: false
     };
   }
 
   componentWillMount() {
+    const story = this.props.story;
     this.setState({
-      name: this.props.story.name
+      name: story.name,
+      description: story.description
     });
   }
 
-  handleStoryNameChange(event) {
-    setState({ name: event.target.value });
+  onInputChange(type, event) {
+    let state = {};
+    state[type] = event.target.value;
+    this.setState(state);
+  }
+
+  onToggleDescriptionEdit() {
+    this.setState({ isEditingDescription: !this.state.isEditingDescription });
   }
 
   render() {
+    let description;
+
+    if (this.state.isEditingDescription) {
+      description = (<textarea value={this.state.description}
+        onChange={this.onInputChange.bind(this)} />);
+    } else {
+      description = (<div onClick={this.onToggleDescriptionEdit.bind(this)}>
+        {this.state.description}</div>)
+    }
+
     return (
       <form>
           <input type="text" value={this.state.name}
-            onChange={this.handleStoryNameChange.bind(this)} />
+            onChange={this.onInputChange.bind(this)} />
+          {description}
       </form>
     );
   }
