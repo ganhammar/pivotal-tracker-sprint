014ba87964db3c46394c07fdb752a65f9d545b12
Anton Ganhammar <ganhammar@gmail.com>
2017-04-07 21:47:49 +0200

    Comments styling and create comments

--- src/api/CommentApi.js
+++ src/api/CommentApi.js
@@ -1,7 +1,10 @@
 import Base from './Base';
 
 class CommentApi extends Base {
-
+  post (projectId, storyId, body) {
+    return this._post(`projects/${projectId}/stories/${storyId}/comments`,
+      body);
+  }
 }
 
 const commentApi = new CommentApi();
