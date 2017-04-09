2d9456c4a9de94c42f065743f57866117b828cb3
Anton Ganhammar <ganhammar@gmail.com>
2017-04-09 21:43:46 +0200

    Login page updates

--- src/views/Login.js
+++ src/views/Login.js
@@ -5,6 +5,7 @@ import Loading from './../components/Loading';
 import Notification from './../components/Notification';
 import Me from './../api/Me';
 import TrackerStore from './../stores/TrackerStore';
+import './../styles/login.scss';
 
 class Login extends React.Component {
   constructor() {
@@ -68,6 +69,26 @@ class Login extends React.Component {
       });
   }
 
+  toggleActive(event) {
+    const prevActive = document.querySelectorAll("#sliders nav ul li.active")[0];
+    prevActive.classList.remove("active");
+    prevActive.classList.add("inactive");
+
+    event.target.classList.add("active");
+
+    const target = event.target.dataset.target;
+      document.querySelectorAll("#sliders .slide")
+      	.forEach((nav) => {
+        	if (nav.id === target) {
+          	nav.classList.add("active");
+            nav.classList.remove("inactive");
+          } else {
+          	nav.classList.remove("active");
+            nav.classList.add("inactive");
+          }
+        });
+  }
+
   render() {
     let hint;
 
@@ -81,14 +102,43 @@ class Login extends React.Component {
     }
 
     return (
-      <form className="login-form">
-        <input type="text" value={this.state.username}
-          onChange={this.handleUsernameChange.bind(this)} placeholder="Username" />
-        <input type="password" value={this.state.password}
-          onChange={this.handlePasswordChange.bind(this)} placeholder="Password" />
-        {hint}
-        <input type="submit" value="Login" onClick={this.login.bind(this)} />
-      </form>
+      <div id="sliders">
+        <nav>
+          <ul>
+            <li onClick={this.toggleActive.bind(this)} data-target="register">Register</li>
+            <li onClick={this.toggleActive.bind(this)} data-target="login" className="active">Login</li>
+            <li onClick={this.toggleActive.bind(this)} data-target="apitoken">API Token</li>
+          </ul>
+        </nav>
+        <div className="slide" id="register">
+          <div className="slide__content">
+            <h2>
+              Register
+            </h2>
+            <p>
+              Go to PivotalTracker to register a new account and then come back here to login.
+            </p>
+            <a className="button neutral" href="http://www.pivotaltracker.com/">www.pivotaltracker.com</a>
+          </div>
+        </div>
+        <div className="slide active" id="login">
+          <form className="slide__content login-form">
+            <p>Login using your PivotalTracker credentials.</p>
+            <input type="text" value={this.state.username}
+              onChange={this.handleUsernameChange.bind(this)} placeholder="Username" />
+            <input type="password" value={this.state.password}
+              onChange={this.handlePasswordChange.bind(this)} placeholder="Password" />
+            {hint}
+            <input type="submit" value="Login" onClick={this.login.bind(this)} />
+          </form>
+        </div>
+        <div className="slide" id="apitoken">
+          <form className="slide__content">
+            <input type="text" placeholder="API Token" />
+            <input type="submit" className="button positive" value="Login" />
+          </form>
+        </div>
+      </div>
     );
   }
 }
