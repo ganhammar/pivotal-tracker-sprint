633d6c3c36872b332317a1672e7837965b3326de
Anton Ganhammar <anton.ganhammar@sca.com>
2017-04-11 16:07:14 +0200

    Polling in progress

--- src/utils/PopulateTrackerStore.js
+++ src/utils/PopulateTrackerStore.js
@@ -86,11 +86,48 @@ function pollStories() {
           stories = stories.concat(result);
 
           if (updatedProjects === projects.length) {
-            // Determine
+            updateStories(stories);
+            pollStories();
           }
         });
     });
   }, 30000);
+}
+
+function updateStories(updates) {
+  let current = TrackerStore.stories;
+  let i;
+  let found;
+
+  updates.forEach((update) => {
+    found = false;
+
+    for (i = 0; i < current.length; i++) {
+      if (update.id === current[i].id) {
+        found = true;
+
+        current[i] = update;
+      }
+    }
+
+    if (found === false) {
+      current.push(update);
+    }
+  });
+
+  for (i = 0; i < current.length; i++) {
+    const story = updates.find((update) => {
+      if (update.id === current[i].id) {
+        return true;
+      }
+    });
+
+    if (!story) {
+      current.splice(i, 1);
+    }
+  }
+
+  TrackerStore.stories = current;
 }
 
 function populateMembers(projectId) {
