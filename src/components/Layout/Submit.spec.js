2280949fafab53205d6d2090b7ce06acbaf886c5
Anton Ganhammar <anton.ganhammar@sca.com>
2017-04-26 08:02:01 +0200

    Updated submit spec to cover most of the functionallity

--- src/components/Layout/Submit.spec.js
+++ src/components/Layout/Submit.spec.js
@@ -22,7 +22,7 @@ describe('Submit', () => {
     jest.useFakeTimers();
 
     const callback = jest.fn(() => {
-      var promise = new Promise((resolve) => {
+      const promise = new Promise((resolve) => {
         resolve();
         expect(setTimeout.mock.calls.length).toBe(1);
         expect(classList.classes).toEqual(['not-loading']);
@@ -35,12 +35,55 @@ describe('Submit', () => {
 
       return promise;
     });
-    const submit = shallow(<Submit text="Test Button" callback={callback} />);
+    const submit = shallow(<Submit callback={callback} />);
     const target = submit.find('.text');
 
     expect(callback).not.toBeCalled();
     expect(classList.classes).toEqual([]);
 
     target.simulate('click', {target: { parentElement: { classList } }});
+  });
+
+  it ('sets the passed in text content', () => {
+    const callback = jest.fn(() => new Promise((resolve) => resolve()));
+    const text = 'Text content is correct';
+    const submit = shallow(<Submit text={text} callback={callback} />);
+
+    expect(submit.find('.text').text()).toEqual(text);
+  });
+
+  it ('sets the passed in classes', () => {
+    const callback = jest.fn(() => new Promise((resolve) => resolve()));
+    const className = 'class-exists';
+    const submit = shallow(<Submit class={className} callback={callback} />);
+
+    expect(submit.hasClass(className)).toEqual(true);
+  });
+
+  it ('sets the is disabled class if it should be disabled', () => {
+    const callback = jest.fn(() => new Promise((resolve) => resolve()));
+    const className = 'disabled';
+    const submit = shallow(<Submit isDisabled={true} callback={callback} />);
+
+    expect(submit.hasClass(className)).toEqual(true);
+
+    submit.setProps({isDisabled: false});
+
+    expect(submit.hasClass(className)).toEqual(false);
+  });
+
+  it ('shouldn\'t call callback if it is disabled', () => {
+    const callback = jest.fn(() => new Promise((resolve) => resolve()));
+    const submit = shallow(<Submit isDisabled={true} callback={callback} />);
+
+    submit.find('.text').simulate('click');
+
+    expect(callback).not.toBeCalled();
+
+    submit.setProps({isDisabled: false});
+
+    submit.find('.text').simulate('click', {target: { parentElement: { classList } }});
+
+    expect(callback.mock.calls.length).toBe(1);
   });
 });
